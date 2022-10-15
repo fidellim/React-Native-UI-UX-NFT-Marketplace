06d8f9c8366608207d33ea39f75f5e77615f1b1f
fidellim <lim.fidel@gmail.com>
2022-10-15 21:08:57 +0400

    add people component in subinfo

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,6 +1,32 @@
 import { View, Image, Text } from 'react-native'
 import { SIZES, FONTS, COLORS, SHADOWS, assets } from '../constants'
 
+const ImageCmp = ({ imgUrl, index }) => {
+  return (
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font,
+      }}
+    />
+  )
+}
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {[assets.person02, assets.person03, assets.person04].map(
+        (imgUrl, index) => (
+          <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+        )
+      )}
+    </View>
+  )
+}
+
 export const SubInfo = () => {
   return (
     <View
@@ -12,7 +38,7 @@ export const SubInfo = () => {
         justifyContent: 'space-between',
       }}
     >
-      <Text>Hi</Text>
+      <People />
     </View>
   )
 }
