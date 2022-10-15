de120a5c5eb833a8975ab2b66aa2722e2d80f85b
fidellim <lim.fidel@gmail.com>
2022-10-15 23:58:01 +0400

    format filde

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useFonts } from "expo-font";
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
 
-import Home from "./screens/Home";
-import Details from "./screens/Details";
+import Home from './screens/Home'
+import Details from './screens/Details'
 
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: "transparent",
+    background: 'transparent',
   },
-};
+}
 
 // Like React Router
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const App = () => {
   const [loaded] = useFonts({
-    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
-    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-    InterLight: require("./assets/fonts/Inter-Light.ttf"),
-  });
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+  })
 
-  if (!loaded) return null;
+  if (!loaded) return null
 
   return (
     <NavigationContainer theme={theme}>
@@ -40,7 +40,7 @@ const App = () => {
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
-export default App;
+export default App
